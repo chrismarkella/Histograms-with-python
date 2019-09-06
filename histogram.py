f77ca4b213ed9f6a5a2d0ec4a6d121e0e6f9a15f
Krisztian Markella <chris.markella@gmail.com>
2019-09-06 13:20:08 -0700

    histogram(lst) function created

--- histogram.py
+++ histogram.py
@@ -6,4 +6,11 @@ def freq_counter(lst):
     freq_dict[e] = freq_dict.get(e, 0) + 1
   return freq_dict
 
+def histogram(lst):
+  freq_dict = freq_counter(lst)
+  for tup in freq_dict.items():
+    print(tup[0], "*" * tup[1])
+
+print(lst)
 print(freq_counter(lst))
+histogram(lst)
