a8af0eb8b899d438edcdd5d0377cc3a8e7e357d5
Krisztian Markella <chris.markella@gmail.com>
2019-09-06 13:55:20 -0700

    top_freq(lst: list, rank = 1) -> tuple implemented

--- histogram.py
+++ histogram.py
@@ -1,3 +1,6 @@
+# Following the Tutorials from Real Python
+# https://realpython.com/courses/python-histograms/
+
 import random
 
 lst = [random.randint(0,10) for _ in range(20)]
@@ -16,6 +19,15 @@ def histogram(lst: list, reverse_order:bool = False) -> None:
   for tup in sorted_dict_by_values:
     print('%2d:2 %s' %(tup[0], "+" * tup[1]))
 
+def top_freq(lst: list, rank = 1) -> tuple:
+  freq_dict = freq_counter(lst)
+
+  sorted_dict_by_values = sorted(freq_dict.items(), key = lambda tup: tup[1], reverse = True)
+  return sorted_dict_by_values[rank - 1]  
+
 print(lst)
 print(freq_counter(lst))
 histogram(lst, True)
+
+print(top_freq(lst))
+print(top_freq(lst, 2))
