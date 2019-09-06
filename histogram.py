ff0275309f7bc3aac5affe7caeb23279960a5da9
Krisztian Markella <chris.markella@gmail.com>
2019-09-06 13:30:30 -0700

    sorting order is implemented in histogram

--- histogram.py
+++ histogram.py
@@ -8,14 +8,14 @@ def freq_counter(lst):
     freq_dict[e] = freq_dict.get(e, 0) + 1
   return freq_dict
 
-def histogram(lst):
+def histogram(lst, reverse_order = False):
   freq_dict = freq_counter(lst)
 
-  sorted_dict_by_values = sorted(freq_dict.items(), key = lambda tup: tup[1])
+  sorted_dict_by_values = sorted(freq_dict.items(), key = lambda tup: tup[1], reverse = reverse_order)
 
   for tup in sorted_dict_by_values:
     print(tup[0], "*" * tup[1])
 
 print(lst)
 print(freq_counter(lst))
-histogram(lst)
+histogram(lst, True)
