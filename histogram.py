caed409c66864c6eb7fde9fcb1176b82a3393a4c
Krisztian Markella <chris.markella@gmail.com>
2019-09-06 13:42:09 -0700

    Return types are added to the functions. Indentation fixed if numbers are no more than two digits long.

--- histogram.py
+++ histogram.py
@@ -2,19 +2,19 @@ import random
 
 lst = [random.randint(0,10) for _ in range(20)]
 
-def freq_counter(lst):
+def freq_counter(lst: list) -> dict:
   freq_dict = {}
   for e in lst:
     freq_dict[e] = freq_dict.get(e, 0) + 1
   return freq_dict
 
-def histogram(lst, reverse_order = False):
+def histogram(lst: list, reverse_order:bool = False) -> None:
   freq_dict = freq_counter(lst)
 
   sorted_dict_by_values = sorted(freq_dict.items(), key = lambda tup: tup[1], reverse = reverse_order)
 
   for tup in sorted_dict_by_values:
-    print(tup[0], "*" * tup[1])
+    print('%2d:2 %s' %(tup[0], "+" * tup[1]))
 
 print(lst)
 print(freq_counter(lst))
